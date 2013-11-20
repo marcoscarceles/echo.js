02f15e5c3d7c3d1e872af75873757e8c6846e1b4
Marcos Carceles <marcos.carcele@gmail.com>
2013-11-20 21:25:29 +0000

    Learning javascript development basics

---
+++ test/parallel.spec.js
@@ -0,0 +1,8 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).not.toBe(false);
+  });
+});
