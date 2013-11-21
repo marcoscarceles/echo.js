5c70d2d3967c8f9f524e2b5bfb7c0bbd46a8b103
Marcos Carceles <marcos.carcele@gmail.com>
2013-11-21 23:17:19 +0000

    Added triggering event

--- src/parallel.js
+++ src/parallel.js
@@ -9,31 +9,35 @@ Parallel.init = function(ref) {
 	return new Parallel(ref);
 };
 
-var register = function(obj, evt, fnc) {
+Parallel.register = function(element, evt, fnc) {
     // W3C model
-    if (obj.addEventListener) {
-        obj.addEventListener(evt, fnc, false);
+    if (element.addEventListener) {
+        element.addEventListener(evt, fnc, false);
         return true;
     } 
     // Microsoft model
-    else if (obj.attachEvent) {
-        return obj.attachEvent('on' + evt, fnc);
-    }
-    // Browser don't support W3C or MSFT model, go on with traditional
-    else {
-        evt = 'on'+evt;
-        if(typeof obj[evt] === 'function'){
-            // Object already has a function on traditional
-            // Let's wrap it with our own function inside another function
-            fnc = (function(f1,f2){
-                return function(){
-                    f1.apply(this,arguments);
-                    f2.apply(this,arguments);
-                };
-            })(obj[evt], fnc);
-        }
-        obj[evt] = fnc;
-        return true;
+    else if (element.attachEvent) {
+        return element.attachEvent('on' + evt, fnc);
     }
     return false;
+};
+
+Parallel.trigger = function(element, evt) {
+  var eventObj;
+  if (document.createEvent) {
+    eventObj = document.createEvent("HTMLEvents");
+    eventObj.initEvent(evt, true, true);
+  } else {
+    eventObj = document.createEventObject();
+    eventObj.eventType = evt;
+  }
+
+  eventObj.eventName = eventName;
+  eventObj.memo = memo || { };
+
+  if (document.createEvent) {
+    element.dispatchEvent(eventObj);
+  } else {
+    element.fireEvent("on" + eventObj.eventType, eventObj);
+  }
 };
